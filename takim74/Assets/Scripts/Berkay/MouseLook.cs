12137e1280197da87e162c1d5c3c1033b726a77d
Berkay Şahin <92673021+akasnowflake@users.noreply.github.com>
2022-04-29 22:26:19 +0300

    Add First Person Controller

---
+++ takim74/Assets/Scripts/Berkay/MouseLook.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensivity = 100f;
+    private float mouseX;
+    private float mouseY;
+    private Transform playerBody;
+    private float verticalRotation; 
+    
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        
+        playerBody = gameObject.transform.parent;
+    }
+
+    
+    void Update()
+    {
+        GetMouseInput();
+        HorizontalLook();
+        VerticalLook();
+ 
+    }
+    void GetMouseInput()
+    {
+        mouseX = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime; 
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensivity * Time.deltaTime;
+    }
+    void HorizontalLook()
+    {
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+    void VerticalLook()
+    {
+        verticalRotation -= mouseY;
+        verticalRotation = Mathf.Clamp(verticalRotation,-90f, 90f); 
+        transform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+    }
+}
